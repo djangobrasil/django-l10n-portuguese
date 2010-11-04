4c21cfda5fc64ecb1e6c2251179ed0915ef956aa
Guilherme Gondim <semente+github@taurinus.org>
2010-11-04 11:56:28 -0700

    Adicionando um sumário.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,6 @@
+;; Configuração do Emacs para a edição da documentação
+;; (afeta todos os arquivos texto deste repositório)
+
 ((text-mode . ((mode . rst)
 	       (fill-column . 79)
 	       (flyspell-mode . 1))))
